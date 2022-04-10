492d161dcf87276f0784622948be1d100d29be31
arabambi <arababs2015@gmail.com>
2022-04-10 20:42:27 +0100

    student model done

---
+++ server/models/Students.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+
+const studentSchema = mongoose.Schema(
+          {
+                    username: {
+                              type: String,
+                              required: true,
+                              trim: true,
+                    },
+
+                    firstname: {
+                              type: String,
+                              required: true,
+                              trim: true,
+                    },
+
+                    lastname: {
+                              type: String,
+                              required: true,
+                              trim: true,
+                    },
+
+                    class: {
+                              type: String,
+                              required: true,
+                              trim: true,
+                    },
+
+                    email: {
+                              type: String,
+                              required: true,
+                              trim: true,
+
+                              validate(value) {
+                                        if (!value.match(/^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/)) {
+                                                  throw new Error('Email is not valid.');
+                                        }
+                              }
+                    },
+
+                    password: {
+                              type: String,
+                              required: true,
+                              trim: true,
+                              minlength: 6
+                    },
+          },
+
+          {
+                    timestamps: true
+          }
+);
+
+
+const Student = mongoose.model('Student', studentSchema);
+
+module.exports = Student;
