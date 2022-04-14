a2879abcbc5dce81f1857280de64512ed990c5ca
arabambi <arababs2015@gmail.com>
2022-04-14 19:09:10 +0100

    article update done1

--- server/routers/article.js
+++ server/routers/article.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const Article = require('../models/Articles');
 const router = express.Router();
+const Student = require('../models/Students')
 
 
 // create a post
@@ -70,7 +71,46 @@ router.put('/api/post/:id', async (req, res) => {
 
 
 // delete a post by both admin and owner
+router.delete('/api/post/:id', async (req, res) => {
+          const user = await Article.findById(req.body.userId);
+
+          if((req.body.userId === req.params.id) || user.isAdmin) {
+                    try {
+                              await Article.findByIdAndDelete(req.params.id);
+
+                              res.status(200).json('Article has been deleted...');
+                    } catch (err) {
+                              res.status(500).json(err);
+                    }
+          }else {
+                    res.status(401).json('You can delete only your post, unless you are admin');
+          }
+});
+
+
 // a post likes
+// router.get('/api/post/likes/update', async (req, res) => {
+//           const post = await Article.findById(req.body.postId);
+//           const student = await Student.findById(req.body.userId);
+
+//           let totalLikes;
+          
+//           try {
+//                     if((post.likes.includes(student._id))) {
+//                               totalLikes.filter((article) => article.likes !== student._id);
+                              
+//                     }else {
+//                               totalLikes = post['likes'].push(student._id);
+//                     }
+
+
+//                     res.status(200).json(totalLikes);
+//                     console.log(totalLikes)
+//           } catch (err) {
+//                     res.status(500).json(err);
+//                     console.log(err);
+//           }
+// })
 
 // post duration before it's deleted
 
